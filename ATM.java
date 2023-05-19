80337313b35fe2ceebd0d51779b2caebc4a2a1b4
Domshak007 <132787417+Domshak007@users.noreply.github.com>
2023-05-19 11:36:29 +0100

    Update ATM.java
    Adding a class MENU

--- ATM.java
+++ ATM.java
@@ -42,3 +42,47 @@ public class ATMMain {
         menu.run();
     }
 }
+import java.util.Scanner;
+
+public class Menu {
+    private ATM atm;
+    private InputReader inputReader;
+
+    public Menu(ATM atm) {
+        this.atm = atm;
+        this.inputReader = new InputReader();
+    }
+
+    public void run() {
+        System.out.println("\nATM Initialized with initial balance: " + atm.getBalance());
+        System.out.println("Withdrawal Limit: " + atm.getWithdrawalLimit());
+
+        while (true) {
+            System.out.println("\nSelect an option:");
+            System.out.println("1. Withdraw");
+            System.out.println("2. Deposit");
+            System.out.println("3. Check Transaction Count");
+            System.out.println("4. Exit");
+            int choice = inputReader.readInt("Enter your choice: ");
+
+            switch (choice) {
+                case 1:
+                    int withdrawalAmount = inputReader.readInt("Enter withdrawal amount: ");
+                    System.out.println(atm.withdraw(withdrawalAmount));
+                    break;
+                case 2:
+                    int depositAmount = inputReader.readInt("Enter deposit amount: ");
+                    System.out.println(atm.deposit(depositAmount));
+                    break;
+                case 3:
+                    System.out.println("Total transactions: " + atm.getTransactionCount());
+                    break;
+                case 4:
+                    System.out.println("Exiting ATM. Thank you!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+}
