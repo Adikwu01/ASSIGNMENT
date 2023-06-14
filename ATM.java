0e7ebe2562751e1dacd0d2ed23c5c4c96e0da3ea
Adikwu01 <133453550+Adikwu01@users.noreply.github.com>
2023-06-14 16:01:38 +0100

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -57,46 +57,4 @@ public class Menu {
         System.out.println("\nATM Initialized with initial balance: " + atm.getBalance());
         System.out.println("Withdrawal Limit: " + atm.getWithdrawalLimit());
 
-        while (true) {
-            System.out.println("\nSelect an option:");
-            System.out.println("1. Withdraw");
-            System.out.println("2. Deposit");
-            System.out.println("3. Check Transaction Count");
-            System.out.println("4. Exit");
-            int choice = inputReader.readInt("Enter your choice: ");
-
-            switch (choice) {
-                case 1:
-                    int withdrawalAmount = inputReader.readInt("Enter withdrawal amount: ");
-                    System.out.println(atm.withdraw(withdrawalAmount));
-                    break;
-                case 2:
-                    int depositAmount = inputReader.readInt("Enter deposit amount: ");
-                    System.out.println(atm.deposit(depositAmount));
-                    break;
-                case 3:
-                    System.out.println("Total transactions: " + atm.getTransactionCount());
-                    break;
-                case 4:
-                    System.out.println("Exiting ATM. Thank you!");
-                    System.exit(0);
-                default:
-                    System.out.println("Invalid choice. Please try again.");
-            }
-        }
-    }
-}
-import java.util.Scanner;
-
-public class InputReader {
-    private Scanner scanner;
-
-    public InputReader() {
-        this.scanner = new Scanner(System.in);
-    }
-
-    public int readInt(String message) {
-        System.out.print(message);
-        return scanner.nextInt();
-    }
-}
+      
